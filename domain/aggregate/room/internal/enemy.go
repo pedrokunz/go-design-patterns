1ce5fc86db8533d60cbc33c7311500b723b315ea
Pedro Kunz <pedro.kunz@gmail.com>
2025-03-08 12:04:34 -0300

    feat: add factory to room

---
+++ domain/aggregate/room/internal/enemy.go
@@ -0,0 +1,23 @@
+package internal
+
+import (
+	"github.com/pedrokunz/go-design-patterns/domain/core/enemy"
+	"github.com/pedrokunz/go-design-patterns/domain/core/item"
+)
+
+type EnemyRoom struct {
+	items   []item.Item
+	enemies []enemy.Enemy
+}
+
+func NewEnemyRoom(items []item.Item, enemies []enemy.Enemy) *EnemyRoom {
+	return &EnemyRoom{items: items, enemies: enemies}
+}
+
+func (e *EnemyRoom) Items() []item.Item {
+	return e.items
+}
+
+func (e *EnemyRoom) Enemies() []enemy.Enemy {
+	return e.enemies
+}
--- domain/aggregate/room/room.go
+++ domain/aggregate/room/room.go
@@ -5,16 +5,7 @@ import (
 	"github.com/pedrokunz/go-design-patterns/domain/core/item"
 )
 
-type Room struct {
-	Items   []item.Item
-	Enemies []enemy.Enemy
-	Kind    Kind
-}
-
-func New(items []item.Item, enemies []enemy.Enemy, kind Kind) *Room {
-	return &Room{
-		Items:   items,
-		Enemies: enemies,
-		Kind:    kind,
-	}
+type Room interface {
+	Items() []item.Item
+	Enemies() []enemy.Enemy
 }
