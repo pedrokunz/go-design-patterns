a5ba86a4d86b89a1694e840ce149f305d7a83c6f
Pedro Kunz <pedro.kunz@gmail.com>
2025-03-29 12:05:02 -0300

    test: add unit tests to notifier
    - fails
      - when notifying observers with error

--- event/observer/notifier_test.go
+++ event/observer/notifier_test.go
@@ -91,6 +91,109 @@ func TestNotifier(t *testing.T) {
 				"error message should be 'event cannot be nil'",
 			)
 		})
+
+		t.Run("when notifying observers with error", func(t *testing.T) {
+			notifier := observer.NewNotifier()
+
+			mockObserver1WithError := NewMockObserver()
+			mockObserver1WithError.on = func(event event.Event) error {
+				return errors.New("observer error")
+			}
+
+			mockObserver2WithError := NewMockObserver()
+			mockObserver2WithError.on = func(event event.Event) error {
+				return errors.New("observer error 2")
+			}
+
+			mockObserver3WithoutError := NewMockObserver()
+
+			attachErr := notifier.Attach(mockObserver1WithError)
+			require.NoError(t, attachErr, "error attaching observer")
+
+			notifyPlayerJoinedErr := notifier.Notify(event.New(event.PlayerJoined))
+			require.EqualError(
+				t,
+				errors.New("observer error"),
+				notifyPlayerJoinedErr.Error(),
+				"error message should be 'observer error'",
+			)
+
+			require.Equal(t, 1, mockObserver1WithError.onCalls, "Observer should call 1 event")
+			require.Len(t, mockObserver1WithError.events, 1, "Observer should have received 1 event")
+			require.Equal(
+				t,
+				event.PlayerJoined,
+				mockObserver1WithError.events[0].Type(),
+				"Observer should have received player joined event",
+			)
+
+			attachErr = notifier.Attach(mockObserver2WithError)
+			require.NoError(t, attachErr, "error attaching observer 2")
+
+			notifyPlayerJoined2Err := notifier.Notify(event.New(event.PlayerJoined))
+			require.EqualError(
+				t,
+				errors.New("observer error; observer error 2"),
+				notifyPlayerJoined2Err.Error(),
+				"error message should be 'observer error; observer error 2'",
+			)
+
+			require.Equal(t, 1, mockObserver2WithError.onCalls, "Observer should call 1 event")
+			require.Len(t, mockObserver2WithError.events, 1, "Observer should have received 1 event")
+			require.Equal(
+				t,
+				event.PlayerJoined,
+				mockObserver2WithError.events[0].Type(),
+				"Observer should have received player joined event",
+			)
+
+			require.Equal(t, 2, mockObserver1WithError.onCalls, "Observer should call 2 events")
+			require.Len(t, mockObserver1WithError.events, 2, "Observer should have received 2 events")
+			require.Equal(
+				t,
+				event.PlayerJoined,
+				mockObserver1WithError.events[1].Type(),
+				"Observer should have received player joined event",
+			)
+
+			attachErr = notifier.Attach(mockObserver3WithoutError)
+			require.NoError(t, attachErr, "error attaching observer 3")
+
+			notifyPlayerLeftErr := notifier.Notify(event.New(event.PlayerLeft))
+			require.EqualError(
+				t,
+				errors.New("observer error; observer error 2"),
+				notifyPlayerLeftErr.Error(),
+				"error message should be 'observer error; observer error 2'",
+			)
+
+			require.Equal(t, 1, mockObserver3WithoutError.onCalls, "Observer should call 1 event")
+			require.Len(t, mockObserver3WithoutError.events, 1, "Observer should have received 1 event")
+			require.Equal(
+				t,
+				event.PlayerLeft,
+				mockObserver3WithoutError.events[0].Type(),
+				"Observer should have received player left event",
+			)
+
+			require.Equal(t, 3, mockObserver1WithError.onCalls, "Observer should call 3 events")
+			require.Len(t, mockObserver1WithError.events, 3, "Observer should have received 3 events")
+			require.Equal(
+				t,
+				event.PlayerLeft,
+				mockObserver1WithError.events[2].Type(),
+				"Observer should have received player left event",
+			)
+
+			require.Equal(t, 2, mockObserver2WithError.onCalls, "Observer should call 2 events")
+			require.Len(t, mockObserver2WithError.events, 2, "Observer should have received 2 events")
+			require.Equal(
+				t,
+				event.PlayerLeft,
+				mockObserver2WithError.events[1].Type(),
+				"Observer should have received player left event",
+			)
+		})
 	})
 }
 
